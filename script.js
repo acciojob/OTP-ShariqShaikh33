d8bf4853065af09c36cab33e8009f62c3b360327
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-10-30 22:16:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ var elts = document.getElementsByClassName('code')
 Array.from(elts).forEach(function(elt){
   elt.addEventListener("keyup", function(event) {
     // Number 13 is the "Enter" key on the keyboard
-    if (elt.value.length == 3) {
+    if (event.keyCode === 13 || elt.value.length == 1) {
       // Focus on the next sibling
-      elt.nextElementSibling.focus()
+      elt.nextElementSibling.focus();
     }
+	if(event.keyCode === 8){
+		elt.previousElementSibling.focus();
+	}
   });
 })
