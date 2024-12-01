f5886962061d53113e59a8c1d626ce80c81d3a39
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-12-01 21:55:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ Array.from(elts).forEach(function(elt){
     if (event.key >= 0 && event.key <= 9) { // if the key is a number
       setTimeout(() => { // wait until the input value is updated
         if (elt.value.length === 1) { // if the input field is filled
-          // Focus on the next sibling
+          // Check if the next sibling exists before focusing
           if (elt.nextElementSibling) {
             elt.nextElementSibling.focus();
           }
@@ -12,7 +12,7 @@ Array.from(elts).forEach(function(elt){
       }, 10);
     }
     if (event.key === 'Backspace' && elt.value.length === 0) { // if backspace is pressed and the input field is empty
-      // Focus on the previous sibling
+      // Check if the previous sibling exists before focusing
       if (elt.previousElementSibling) {
         elt.previousElementSibling.focus();
       }
