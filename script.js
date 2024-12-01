356c24ba943f339dad762cca8aed72ca2b272bfa
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-12-01 21:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-//your JS code here. If required.
 var elts = document.getElementsByClassName('code')
 Array.from(elts).forEach(function(elt){
-  elt.addEventListener("keyup", function(event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13 || elt.value.length == 1) {
-      // Focus on the next sibling
-      elt.nextElementSibling.focus();
+  elt.addEventListener("keydown", function(event) {
+    if (event.key >= 0 && event.key <= 9) { // if the key is a number
+      setTimeout(() => { // wait until the input value is updated
+        if (elt.value.length === 1) { // if the input field is filled
+          // Focus on the next sibling
+          if (elt.nextElementSibling) {
+            elt.nextElementSibling.focus();
+          }
+        }
+      }, 10);
     }
-	if(event.keyCode === 8){
-		elt.previousElementSibling.focus();
-	}
+    if (event.key === 'Backspace' && elt.value.length === 0) { // if backspace is pressed and the input field is empty
+      // Focus on the previous sibling
+      if (elt.previousElementSibling) {
+        elt.previousElementSibling.focus();
+      }
+    }
   });
-})
+});
